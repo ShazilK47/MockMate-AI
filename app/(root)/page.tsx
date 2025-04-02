ca0e62ffca8cc91c0c0cf66f74cba2d92084c71c
ShazilK47 <shazil.akn@gmail.com>
2025-04-03 00:07:06 +0500

    homepage setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -10,7 +12,7 @@ const page = () => {
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice $ Feedback</h2>
           <p className="text-lg">
-            Practice on real interview questions $ get instant feedback
+            Practice on real interview questions & get instant feedback
           </p>
 
           <Button asChild className="btn-primary max-sm:w-full ">
@@ -31,13 +33,19 @@ const page = () => {
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
-          <p>You haven&apos;t taken any interviews yets</p>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p>You haven&apos;t taken any interviews yet</p> */}
         </div>
 
         <section className="flex flex-col gap-6 mt-8 ">
           <h2>Take an Interview</h2>
           <div className="interviews-section">
-            <p>There are no interviews available</p>
+            {dummyInterviews.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))}
+            {/* <p>There are no interviews available</p> */}
           </div>
         </section>
       </section>
